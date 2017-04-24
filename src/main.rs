5fba6ff91ef6b624e94f2daef6185b5fe9a91434
François Simond <dev@supercur.io>
2017-04-24 03:02:14 +0200

    Adding where the sample code comes from

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// From https://github.com/alexcrichton/tokio-signal/blob/master/examples/log.rs
+
 extern crate futures;
 extern crate tokio_core;
 extern crate tokio_signal;
@@ -11,23 +13,25 @@ use tokio_core::reactor::Handle;
 use tokio_io::{IoStream, IoFuture};
 use tokio_signal::unix;
 
-
+// from https://github.com/alexcrichton/tokio-signal/blob/master/src/lib.rs#L54
 fn sigint_imp(handle: &Handle) -> IoFuture<IoStream<()>> {
     unix::Signal::new(unix::libc::SIGINT, handle)
         .map(|x| x.map(|_| ()).boxed())
         .boxed()
 }
+
 fn sigterm_imp(handle: &Handle) -> IoFuture<IoStream<()>> {
     unix::Signal::new(unix::libc::SIGTERM, handle)
         .map(|x| x.map(|_| ()).boxed())
         .boxed()
 }
 
+
 fn main() {
     let mut core = Core::new().unwrap();
     let sigint = sigint_imp(&core.handle());
     let sigterm = sigint_imp(&core.handle());
-    
+
     let stream = core.run(sigint).unwrap();
 
     core.run(stream.for_each(|()| {
